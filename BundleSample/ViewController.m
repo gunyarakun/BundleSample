d8f83618ca178de959c3199ac2adc5d32eb6bf85
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-04-02 00:08:44 -0700

    Use c function

--- BundleSample/ViewController.m
+++ BundleSample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#include <stdio.h>
+#include <sys/stat.h>
 
 @interface ViewController ()
 
@@ -18,19 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+    // Fetch file (obj-c)
     NSBundle *frameworkBundle = [[NSBundle alloc] initWithPath:@"/frameworks/CoreFoundation.framework/Resources"];
     NSString *path = [frameworkBundle pathForResource:@"CFUniCharPropertyDatabase" ofType:@"data"];
+    NSError *error0 = nil;
+    NSString *bundleData = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error: &error0];
 
-    NSFileManager *fm = [NSFileManager defaultManager];
-    NSDictionary *attribute = [fm attributesOfItemAtPath:path error:nil];
-
-    NSNumber *fileSize = [attribute objectForKey:NSFileSize];
+    // Fetch file (c)
+    int fd = open("/frameworks/CoreFoundation.framework/Resources/CFUniCharPropertyDatabase.data", O_RDONLY);
+    struct stat st;
+    fstat(fd, &st);
+    close(fd);
 
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
     label.backgroundColor = [UIColor clearColor];
     label.font = [UIFont systemFontOfSize:30];
 
-    label.text = [NSString stringWithFormat:@"size: %@", fileSize];
+    label.text = [NSString stringWithFormat:@"size: %lu %u", (unsigned long)[bundleData length], st.st_size];
     [label sizeToFit];
     [self.view addSubview:label];
 }
